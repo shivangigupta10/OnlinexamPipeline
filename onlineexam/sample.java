ed3794cc22ec670409e4c1342e67c4aa61083186
AmitGawali4 <39050953+AmitGawali4@users.noreply.github.com>
2020-05-13 21:36:28 +0530

    Update sample.java

--- onlineexam/sample.java
+++ onlineexam/sample.java
@@ -21,7 +21,16 @@ ResultSet rs;
 int i=0;
 int marks=0;
 int j=0;
-
+extArea t1;
+Checkbox c1,c2,c3;
+CheckboxGroup g;
+Button b1,b2;
+Connection c;
+Statement s;
+ResultSet rs;
+int i=0;
+int marks=0;
+int j=0;
 public static void main(String args[]) throws Exception 
 {
 sample s= new sample();	// Create a new frame 
