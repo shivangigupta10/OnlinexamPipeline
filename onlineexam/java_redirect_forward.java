843e4ddb26ce8003b070e560a6ee29bea20b6080
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-05-27 23:13:47 +0530

    Update java_redirect_forward.java

--- onlineexam/java_redirect_forward.java
+++ onlineexam/java_redirect_forward.java
@@ -1,5 +1,7 @@
 <%
 	String redirectURL = "http://www.example.com/";
+public void windowDeiconified(WindowEvent e) {  }
+public void windowClosing(WindowEvent e) {   System.exit(0); }
 	response.sendRedirect(redirectURL);
 	response.sendForward(redirectURL);
 	return;
