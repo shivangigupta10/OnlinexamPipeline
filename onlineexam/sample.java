0426b80b7bea6bfa0f87b187a53bcfa034ee36f7
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-05-28 19:33:30 +0530

    Update sample.java

--- onlineexam/sample.java
+++ onlineexam/sample.java
@@ -1,3 +1,13 @@
+Newly added SQL Injection: 4 scenarios
+
+st.executeQuery("INSERT into student values("+id+",'"+name+"',"+phone+",'"+pwd+"')");
+st.executeQuery("SELECT accountNumber, balance FROM accounts WHERE account_owner_id = " + request.getParameter("user_id")");
+st.executeQuery("UPDATE question set q_id="+qid+",ques='"+quest+"',ans="+ans+",author='"+author+"',op1='"+op1+"',op2='"+op2+"',op3='"+op3+"',op4='"+op4+"' where q_id="+qid");
+st.executeQuery("DELETE from question where q_id="+qid);
+
+
+
+
 Newly Added Poor logging add :
 
 System.out.println("Generation password reset link for " + username);
