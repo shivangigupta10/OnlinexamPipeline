dd190b92857387903ccfedc9da3b48b74bb6ab3e
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-07-07 16:57:14 +0530

    Add files via upload

---
+++ onlineexam/TestFileCIODEVJs.js
@@ -0,0 +1,10 @@
+dangerouslySetInnerHTML new
+abc httpProvider new.withCredentials=false 
+angular.module("new")
+React.createElement(abc,bvx,xyz)
+---------------------------------------------------
+//dangerouslySetInnerHTML new
+//abc httpProvider new.withCredentials=false 
+//angular.module("new")
+//React.createElement(abc,bvx,xyz)
+----------------------------------------------------
