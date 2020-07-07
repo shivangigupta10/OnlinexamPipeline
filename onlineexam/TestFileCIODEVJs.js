12d348863039ec8995c711a90bd74003c179fc28
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-07-07 18:01:28 +0530

    Update TestFileCIODEVJs.js

--- onlineexam/TestFileCIODEVJs.js
+++ onlineexam/TestFileCIODEVJs.js
@@ -1,10 +1,39 @@
+Improper Neutralization 
+
 dangerouslySetInnerHTML new
 abc httpProvider new.withCredentials=false 
 angular.module("new")
 React.createElement(abc,bvx,xyz)
 ---------------------------------------------------
+Improper Neutralization PFP
+
 //dangerouslySetInnerHTML new
 //abc httpProvider new.withCredentials=false 
 //angular.module("new")
 //React.createElement(abc,bvx,xyz)
 ----------------------------------------------------
+Cross site scripting
+
+esc_js($
+abc.html(new).text( )
+Document.write ())
+Document.writeln ())
+new=Window.location.href
+Window.location.href=new
+abc.innerHTML=mno
+nativeElement
+new.outerHTML=mnop
+window.__PRELOADED_STATE__=${JSON.Stringify(preloadedState)}
+--------------------------------------------------------------------
+Cross site scripting PFP
+
+//esc_js($
+//abc.html(new).text( )
+//Document.write ())
+//Document.writeln ())
+//new=Window.location.href
+//Window.location.href=new
+//abc.innerHTML=mno
+//nativeElement
+//new.outerHTML=mnop
+//window.__PRELOADED_STATE__=${JSON.Stringify(preloadedState)}
