d4ed7fb32db41aee7e38142ad66b4ccc401a1473
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-07-07 14:24:11 +0530

    Update TestFileCIODEV.java

--- onlineexam/TestFileCIODEV.java
+++ onlineexam/TestFileCIODEV.java
@@ -1,3 +1,20 @@
 dangerouslySetInnerHTML 
 httpProvider new.withCredentials=false
 angular.module("new")
+
+-----------------------------------------------
+
+a.setHttpOnly(false)
+a.setHttpOnly(False)
+a.secure(false)
+a.secure(False)
+session.cookie_secure=false
+session.cookie_secure=False
+Secure=false
+Secure=False
+<secure> false </secure>
+<secure> False </secure>
+<http-only>False</http-only>
+<http-only>false</http-only>
+httpOnlyCookies="False
+httpOnlyCookies="false"
