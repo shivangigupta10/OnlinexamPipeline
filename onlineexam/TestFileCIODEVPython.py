fa513e7cd60f1dbd1c63c11c89901cba9ee83f61
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-07-08 12:13:24 +0530

    Add files via upload

---
+++ onlineexam/TestFileCIODEVPython.py
@@ -0,0 +1,15 @@
+Key Management: Empty PBE Password
+
+abc pbkdf1_hmac('a', '',salt,
+abc pbkdf2_hmac('a', '',salt,
+-------------------------------------------------------------------------
+Key Management: Hardcoded HMAC Key
+
+hmac.new("a"
+------------------------------------------------------------------------
+Key Management: Null Encryption Key
+
+AES.new(None)
+----------------------------------------------------------------------------
+
+
