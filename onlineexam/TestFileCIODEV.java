ddc52e04af10a7521d0be7eb63c254f6e05a9bad
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-07-08 12:04:34 +0530

    Update TestFileCIODEV.java

--- onlineexam/TestFileCIODEV.java
+++ onlineexam/TestFileCIODEV.java
@@ -1,3 +1,5 @@
+Improper Session Handling
+
 httponly=0
 httponly=False
 httponly=off
@@ -10,6 +12,8 @@ session.use_only_cookies=off
 abc.setMaxInactiveInterval(-1)
 <session-timeout> -1 </session-timeout>
 ------------------------------------------------
+Improper Session Handling PFP
+
 //httponly=0
 //httponly=False
 //httponly=off
@@ -21,3 +25,13 @@ abc.setMaxInactiveInterval(-1)
 //<property name="idLengthBytes" value ="00">
 //abc.setMaxInactiveInterval(-1)
 //<session-timeout> -1 </session-timeout>
+-----------------------------------------------------------------------
+Cryptography
+
+random.random()
+newrandom.random()
+--------------------------------------------------------------------
+Cryptography
+
+new Cryptiles.randomDigits()
+Cryptiles.randomDigits()
