fdfe694822f6379c245ee26229f19742f13b3db1
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-07-09 10:11:28 +0530

    Add files via upload

---
+++ onlineexam/TestFileCIODEV.java
@@ -0,0 +1,100 @@
+Improper Session Handling
+
+httponly=0
+httponly=False
+httponly=off
+Session["abc"]
+Session("abc")
+session.cookie_lifetime=9
+session.use_only_cookies=off
+<property name="idLengthBytes" value ="1">
+<property name="idLengthBytes" value ="00">
+abc.setMaxInactiveInterval(-1)
+<session-timeout> -1 </session-timeout>
+------------------------------------------------
+Improper Session Handling PFP
+
+//httponly=0
+//httponly=False
+//httponly=off
+//Session["abc"]
+//Session("abc")
+//session.cookie_lifetime=9
+//session.use_only_cookies=off
+//<property name="idLengthBytes" value ="1">
+//<property name="idLengthBytes" value ="00">
+//abc.setMaxInactiveInterval(-1)
+//<session-timeout> -1 </session-timeout>
+
+--------------------------------------------------------------------
+Weak Random
+ 
+new Cryptiles.randomDigits()   
+Cryptiles.randomDigits()
+-----------------------------------------------------------------------
+Untrusted Data
+
+log(
+log.WriteEntry(
+log.Error(
+log.Info(
+Eventlog(
+Eventlog.WriteEntry(
+Eventlog.Error(
+Eventlog.Info(
+logger.WriteEntry
+logger.Error
+ log.WriteEntry(
+newlog.Error(
+//log(
+//log.WriteEntry(
+//log.Error(
+//log.Info(
+//Eventlog(
+//Eventlog.WriteEntry(
+//Eventlog.Error(
+//Eventlog.Info(
+//logger.WriteEntry
+//logger.Error
+// log.WriteEntry(
+// newlog.Error(
+---------------------------------------------------------------------------
+Sensitive data exposure web url
+
+abc http://facebook?new=09kjj
+abc https://facebook?new=09kjj
+---------------------------------------------------------------------------
+Denial of service
+
+abc.readLine(
+---------------------------------------------------------------------------------
+Untrusted Data
+
+debug="true"
+trace enabled = "true"
+getIntent(
+//debug="true"
+//trace enabled = "true"
+//getIntent(
+--------------------------------------------------------------------------------------
+Cross site scripting 
+
+abc.getText(
+.getText(
+------------------------------------------------------------------------------------------
+Cross site scripting pfp
+
+//abc.getText(
+//.getText(
+---------------------------------------------------------------------------------------
+Cross site scripting ASAP plugin Deleted ones
+
+require("express") 
+require("x-xss-protection")
+require("helmet")
+
+
+
+
+
+
